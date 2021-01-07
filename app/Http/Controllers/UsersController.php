328883bd444035392147d265be0e42d6c053cd86
冉超 <756620245@qq.com>
2021-01-07 16:15:24 +0800

    裁剪头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -30,13 +30,13 @@ class UsersController extends Controller
         return view('users.edit', compact('user'));
     }
 
-    public function update(UserRequest $request,ImageUploadHandler $uploader, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
         $data = $request->all();
 
-        if($request->avatar){
-            $result = $uploader->save($request->avatar,'avatar',$user->id);
-            if($result){
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatar', $user->id, 300);
+            if ($result) {
                 $data['avatar'] = $result['path'];
             }
         }
