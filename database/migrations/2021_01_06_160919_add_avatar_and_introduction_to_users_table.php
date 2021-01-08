9314691289a880ee46661489f56c5aa76857a83b
冉超 <756620245@qq.com>
2021-01-08 11:10:55 +0800

    填充用户和话题数据

--- database/migrations/2021_01_06_160919_add_avatar_and_introduction_to_users_table.php
+++ database/migrations/2021_01_06_160919_add_avatar_and_introduction_to_users_table.php
@@ -1,16 +1,11 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class AddAvatarAndIntroductionToUsersTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
@@ -19,16 +14,11 @@ class AddAvatarAndIntroductionToUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
             $table->dropColumn('avatar');
-            $table->dropColumn('avatar');
+            $table->dropColumn('introduction');
         });
     }
 }
