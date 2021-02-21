8008df5ac6ed73b581e70ca1ecc34266b4bcab47
冉超 <756620245@qq.com>
2021-02-21 16:29:47 +0800

    删除评论

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,22 +4,13 @@ namespace App\Observers;
 
 use App\Jobs\TranslateSlug;
 use App\Models\Topic;
+use Illuminate\Support\Facades\DB;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 
 class TopicObserver
 {
-    public function creating(Topic $topic)
-    {
-        //
-    }
-
-    public function updating(Topic $topic)
-    {
-        //
-    }
-
     public function saving(Topic $topic)
     {
         $topic->body = clean($topic->body, 'user_topic_body');
@@ -31,5 +22,10 @@ class TopicObserver
         if (!$topic->slug) {
             dispatch(new TranslateSlug($topic));
         }
+    }
+
+    public function deleted(Topic $topic)
+    {
+        DB::table('replies')->where('topic_id', $topic->id)->delete();
     }
 }
