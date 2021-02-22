445c19465e5de0169792d50441eddd897b71f8d7
冉超 <756620245@qq.com>
2021-02-22 11:25:10 +0800

    站点权限部署

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,8 +13,8 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-		 \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
-		 \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
+        \App\Models\Reply::class => \App\Policies\ReplyPolicy::class,
+        \App\Models\Topic::class => \App\Policies\TopicPolicy::class,
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
@@ -32,5 +32,8 @@ class AuthServiceProvider extends ServiceProvider
             //动态返回模型对应的策略名称
             return 'App\Policies\\' . class_basename($modelClass) . 'Policy';
         });
+//        \Horizon::auth(function ($request)){
+//            return \Auth::user()->hasRole('Founder');
+//        }
     }
 }
