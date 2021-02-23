04adac58ecf3780ffe8c1f6d54b960c470f40fe5
冉超 <756620245@qq.com>
2021-02-23 17:02:06 +0800

    侧边栏链接

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
-		$this->call(RepliesTableSeeder::class);
+        $this->call(RepliesTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
         // \App\Models\User::factory(10)->create();
     }
 }
