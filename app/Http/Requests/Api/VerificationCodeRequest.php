d32a149ecd3f88e7237d3a5c391b3875fc83d8f7
冉超 <756620245@qq.com>
2021-03-03 11:23:20 +0800

    图片验证码

--- app/Http/Requests/Api/VerificationCodeRequest.php
+++ app/Http/Requests/Api/VerificationCodeRequest.php
@@ -6,15 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class VerificationCodeRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
 
     /**
      * Get the validation rules that apply to the request.
@@ -24,7 +15,16 @@ class VerificationCodeRequest extends FormRequest
     public function rules()
     {
         return [
-            'phone' => 'required|phone:CN,mobile|unique:users',
+            'captcha_key' => 'required|string',
+            'captcha_code' => 'required|string',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'captcha_key' => '图片验证码 key',
+            'captcha_code' => '图片验证码',
         ];
     }
 }
