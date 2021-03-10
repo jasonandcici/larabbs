5dbcf5143db512883dc5aa39924943b6d572ceb2
冉超 <756620245@qq.com>
2021-03-10 15:50:12 +0800

    回复列表

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,10 @@ Route::prefix('v1')->namespace('Api')->name('api.v1')->group(function () {
             Route::post('topics/{topic}/replies', 'RepliesController@store')->name('topics.replies.store');
             //删除回复
             Route::delete('topics/{topic}/replies/{reply}', 'RepliesController@destroy')->name('topics.replies.destroy');
+            //话题回复列表
+            Route::get('topics/{topic}/relies', 'RepliesController@index')->name('topics.replies.index');
+            //某个用户的回复列表
+            Route::get('users/{user}/replies', 'RepliesController@userIndex')->name('users.replies.index');
         });
     });
 
